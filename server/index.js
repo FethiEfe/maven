c6611bb55a5a08eb1379883dfdfb5c1e75496c39
Fethi E <jack@jacks-MBP-2.satx.rr.com>
2019-08-02 11:09:02 -0500

    very last commit 1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express");
 const session = require("express-session")
+const path = require('path'); 
 const app = express();
 
 const controller = require("./controller")
