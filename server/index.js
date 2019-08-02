f865de4097eb2a0c4f88c539ac1a593e507e7461
Fethi E <jack@jacks-MBP-2.satx.rr.com>
2019-08-02 10:42:24 -0500

    deploy

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const controller = require("./controller")
 var PORT = process.env.PORT || 3001;
 const {SECRET_SESSION} = process.env
 
-app.use(express.static('client'));
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.use(express.json());
 app.use(session({
@@ -27,9 +27,9 @@ if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+  app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+  });
   }
 
 
